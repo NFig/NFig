3be01f019d5adbc5015e1f540cbfc2b2c70e46ff
Bret Copeland <bret@atlantisflight.org>
2015-02-17 02:52:49 -0500

    Started on interface for nfig stores.

---
+++ NFig/SettingInfo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace NFig
+{
+    public class SettingInfo
+    {
+        public string Key { get; internal set; }
+        public string ActiveStringValue { get; internal set; }
+        public string DefaultStringValue { get; internal set; }
+
+        public bool HasOverride { get; internal set; }
+
+        public string GetOverride<TTier, TDataCenter>(TTier? tier, TDataCenter? dataCenter)
+            where TTier : struct
+            where TDataCenter : struct
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
