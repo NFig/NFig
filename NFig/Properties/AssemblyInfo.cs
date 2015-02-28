7b23dd47e002f495a1f918a7dcdf19716c9ac8af
Bret Copeland <bret@atlantisflight.org>
2015-02-28 17:39:15 -0500

    Setup AppVeyor.

--- NFig/Properties/AssemblyInfo.cs
+++ NFig/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("NFig")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("A Settings library supporting multi-tier and data center environments.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("NFig")]
@@ -34,3 +34,4 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyInformationalVersion("1.0.0.0")]
---
+++ Scripts/SetNugetVersion.ps1
@@ -0,0 +1,33 @@
+
+# figure out the correct nuget package version (depends on whether this is a release or not)
+$version = "$env:NUGET_RELEASE_VERSION"
+if ("$env:APPVEYOR_REPO_TAG" -ne "true") # non-tagged (pre-release build)
+{
+  $version += "-unstable$env:APPVEYOR_BUILD_NUMBER"
+}
+
+# grab .nuspec file contents
+$file = "$PSScriptRoot\..\BosunReporter\BosunReporter.nuspec"
+$contents = (Get-Content $file) | Out-String
+
+# replace NUGET_VERSION with the the actual version
+$count = 0
+$replacer = [System.Text.RegularExpressions.MatchEvaluator]{
+  $script:count++
+  "$version"
+}
+$contents = [Regex]::Replace($contents, "NUGET_VERSION", $replacer)
+
+# make sure NUGET_VERSION existed exactly once
+if ($count -ne 1)
+{
+  Write-Error "NUGET_VERSION was found $count times in the nuspec file. If should have been found exactly once."
+  Exit 1
+}
+
+# set the NUGET_VERSION env variable
+[Environment]::SetEnvironmentVariable("NUGET_VERSION", "$version", "User")
+
+# save nuspec file
+Write-Host "Nuget version set as $version"
+$contents | Set-Content $file
