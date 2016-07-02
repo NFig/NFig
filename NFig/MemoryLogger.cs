a096de51a71bea03442d0c29c4d4872ad2254f42
Bret Copeland <bret@atlantisflight.org>
2016-07-02 17:23:08 -0400

    Start implementation of SettingsLogger

---
+++ NFig/MemoryLogger.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace NFig
+{
+    public class MemoryLogger<TTier, TDataCenter> : SettingsLogger<TTier, TDataCenter>
+        where TTier : struct
+        where TDataCenter : struct
+    {
+        private readonly List<AppSnapshot<TTier, TDataCenter>> _history = new List<AppSnapshot<TTier, TDataCenter>>();
+
+        public MemoryLogger(Action<Exception, AppSnapshot<TTier, TDataCenter>> onLogException) : base(onLogException)
+        {
+        }
+
+        protected override Task LogAsyncImpl(AppSnapshot<TTier, TDataCenter> snapshot)
+        {
+            lock (_history)
+            {
+                _history.Add(snapshot);
+            }
+
+            return Task.FromResult(0);
+        }
+
+        protected override Task<AppSnapshot<TTier, TDataCenter>> GetSnapshotAsync(string commit)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ NFig/SettingsLogger.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace NFig
+{
+    public abstract class SettingsLogger<TTier, TDataCenter>
+        where TTier : struct
+        where TDataCenter : struct
+    {
+        private readonly Action<Exception, AppSnapshot<TTier, TDataCenter>> _onLogException;
+
+        protected SettingsLogger(Action<Exception, AppSnapshot<TTier, TDataCenter>> onLogException)
+        {
+            if (onLogException == null)
+                throw new ArgumentNullException(nameof(onLogException));
+
+            _onLogException = onLogException;
+        }
+
+        public void Log(AppSnapshot<TTier, TDataCenter> snapshot)
+        {
+            Task.Run(async () =>
+            {
+                try
+                {
+                    await LogAsyncImpl(snapshot);
+                }
+                catch (Exception e)
+                {
+                    _onLogException(e, snapshot);
+                }
+            });
+        }
+
+        protected abstract Task LogAsyncImpl(AppSnapshot<TTier, TDataCenter> snapshot);
+
+        protected abstract Task<AppSnapshot<TTier, TDataCenter>> GetSnapshotAsync(string commit);
+    }
+}
