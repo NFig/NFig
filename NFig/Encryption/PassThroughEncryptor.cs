c08cf0d8c8b20b6c15755c90babb5919882d3da1
Bret Copeland <bret@atlantisflight.org>
2016-07-04 14:19:06 -0400

    Add PassThroughEncryptor

---
+++ NFig/Encryption/PassThroughEncryptor.cs
@@ -0,0 +1,19 @@
+﻿namespace NFig.Encryption
+{
+    /// <summary>
+    /// The PassThroughEnctyptor doesn't actually perform any encryption or decryption. It may be useful for testing environments or local tier,
+    /// but does not provide any added security at all.
+    /// </summary>
+    public class PassThroughEncryptor : ISettingEncryptor
+    {
+        public string Encrypt(string value)
+        {
+            return value;
+        }
+
+        public string Decrypt(string encryptedValue)
+        {
+            return encryptedValue;
+        }
+    }
+}
