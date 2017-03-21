c16206d485a5dc018ffd77f1213b1445693c234a
Bret Copeland <bret@atlantisflight.org>
2017-03-21 00:26:49 -0400

    Add BySettingDictionary

---
+++ NFig/IBySettingDictionaryItem.cs
@@ -0,0 +1,13 @@
+﻿namespace NFig
+{
+    /// <summary>
+    /// The interface for the values of an <see cref="BySettingDictionary{TValue}"/>.
+    /// </summary>
+    public interface IBySettingDictionaryItem
+    {
+        /// <summary>
+        /// The setting name. This property will become the key in a <see cref="BySettingDictionary{TValue}"/>.
+        /// </summary>
+        string Name { get; }
+    }
+}
