c48bc6499381e69cc3163022aa8fe3680272a14f
Bryan Ross <git@rossipedia.com>
2017-05-08 14:44:18 -0600

    fix #17
    - add a protected constructor to SettingConverterAttribute which accepts an
      instance of ISettingConverter

--- NFig/SettingConverterAttribute.cs
+++ NFig/SettingConverterAttribute.cs
@@ -1,5 +1,6 @@
-﻿using System;
+using System;
 using System.Linq;
+using JetBrains.Annotations;
 
 namespace NFig
 {
@@ -11,7 +12,7 @@ namespace NFig
         /// <summary>
         /// The converter object. It will implement <see cref="ISettingConverter{TValue}"/> where TValue is the property type of the setting.
         /// </summary>
-        public ISettingConverter Converter { get; private set; }
+        public ISettingConverter Converter { get; }
 
         /// <summary>
         /// Explicitly assigns a converter to a specific setting. If you want a converter to automatically apply to any setting of a particular type, pass the
@@ -20,14 +21,38 @@ namespace NFig
         /// <param name="converterType">The type must implement <see cref="ISettingConverter{TValue}"/> where TValue is the property type of the setting.</param>
         public SettingConverterAttribute(Type converterType)
         {
+            ValidateConverterType(converterType);
+            Converter = (ISettingConverter)Activator.CreateInstance(converterType);
+        }
+
+        /// <summary>
+        /// Explicitly assigns a converter to a specific setting. If you want a converter to automatically apply to any setting of a particular type, pass the
+        /// converter as part of the "additionalDefaultConverters" argument to the NFigStore you're using.
+        /// </summary>
+        /// <param name="converter">
+        /// An instance of <see cref="ISettingConverter"/>. The concrete type must implement  <see cref="ISettingConverter{TValue}"/> 
+        /// where TValue is the property type of the setting.
+        /// </param>
+        protected SettingConverterAttribute([NotNull] ISettingConverter converter)
+        {
+            if (converter == null)
+            {
+                throw new ArgumentNullException(nameof(converter));
+            }
+
+            ValidateConverterType(converter.GetType());
+
+            Converter = converter;
+        }
+
+        static void ValidateConverterType(Type converterType)
+        {
             // make sure type implements SettingsConverter<>
             var genericType = typeof(ISettingConverter<>);
             if (!converterType.GetInterfaces().Any(i => i.IsGenericType && i.GetGenericTypeDefinition() == genericType))
             {
                 throw new InvalidOperationException($"Cannot use type {converterType.Name} as a setting converter. It does not implement ISettingConverter<T>.");
             }
-
-            Converter = (ISettingConverter)Activator.CreateInstance(converterType);
         }
     }
 }
