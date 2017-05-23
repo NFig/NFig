4f40a3cebdbacfc722e051279255f83e1d13023b
Bret Copeland <bret@atlantisflight.org>
2017-05-23 18:31:11 -0400

    Add IsDefaultConverter to SettingMetadata
    Related to #9

--- NFig/SettingMetadata.cs
+++ NFig/SettingMetadata.cs
@@ -12,40 +12,45 @@ namespace NFig
         /// The name of the setting. Dots in the name represent nesting levels.
         /// </summary>
         public string Name { get; }
-
         /// <summary>
         /// User-provided description of the setting.
         /// </summary>
         public string Description { get; }
-
         /// <summary>
         /// The full name of the setting's type.
         /// </summary>
         public string TypeName { get; }
-
         /// <summary>
         /// If true, all default and override values are encrypted.
         /// </summary>
         public bool IsEncrypted { get; }
-
         /// <summary>
         /// True if the setting type is an enum. Use integer values when setting overrides for enum settings (e.g. to assign the value MyEnum.Two, use "2", not
         /// "Two").
         /// </summary>
         public bool IsEnum { get; }
-
         /// <summary>
         /// The fully-qualified type name of the setting's converter. All built-in converters begin with "NFig.Converters."
         /// </summary>
         public string ConverterTypeName { get; }
-
+        /// <summary>
+        /// True if this setting uses the built-in default converter for the setting's type.
+        /// </summary>
+        public bool IsDefaultConverter { get; }
         /// <summary>
         /// True if the setting was marked with the [ChangeRequiresRestart] attribute. This indicates that any edits to this setting likely won't have any
         /// affect until the application is restarted.
         /// </summary>
         public bool ChangeRequiresRestart { get; }
 
-        internal SettingMetadata(string name, string description, Type type, bool isEncrypted, ISettingConverter converter, bool changeRequiresRestart)
+        internal SettingMetadata(
+            string name,
+            string description,
+            Type type,
+            bool isEncrypted,
+            ISettingConverter converter,
+            bool isDefaultConverter,
+            bool changeRequiresRestart)
         {
             Name = name;
             Description = description;
@@ -53,6 +58,7 @@ namespace NFig
             IsEncrypted = isEncrypted;
             IsEnum = type.IsEnum();
             ConverterTypeName = converter.GetType().FullName;
+            IsDefaultConverter = isDefaultConverter;
             ChangeRequiresRestart = changeRequiresRestart;
         }
     }
