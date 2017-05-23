0b33a714e3710c243f879e728bfcaad617ce5286
Bret Copeland <bret@atlantisflight.org>
2017-05-22 23:16:53 -0400

    Add DefaultConverters static class
    This is probably temporary. I'm already thinking about a better way to
    represent the converters of an app.

---
+++ NFig/Converters/DefaultConverters.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using JetBrains.Annotations;
+
+namespace NFig.Converters
+{
+    static class DefaultConverters
+    {
+        static readonly Dictionary<Type, ISettingConverter> s_defaultConverters = new Dictionary<Type, ISettingConverter>
+        {
+            [typeof(bool)] = new BooleanSettingConverter(),
+            [typeof(byte)] = new ByteSettingConverter(),
+            [typeof(short)] = new ShortSettingConverter(),
+            [typeof(ushort)] = new UShortSettingConverter(),
+            [typeof(int)] = new IntSettingConverter(),
+            [typeof(uint)] = new UIntSettingConverter(),
+            [typeof(long)] = new LongSettingConverter(),
+            [typeof(ulong)] = new ULongSettingConverter(),
+            [typeof(float)] = new FloatSettingConverter(),
+            [typeof(double)] = new DoubleSettingConverter(),
+            [typeof(string)] = new StringSettingConverter(),
+            [typeof(char)] = new CharSettingConverter(),
+            [typeof(decimal)] = new DecimalSettingConverter(),
+        };
+
+        [CanBeNull]
+        static ISettingConverter Get(Type settingType)
+        {
+            return s_defaultConverters.TryGetValue(settingType, out var converter) ? converter : null;
+        }
+    }
+}
