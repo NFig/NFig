db638843db28de3bcccaa306c0dbcf2da342e39d
Bret Copeland <bret@atlantisflight.org>
2017-05-22 23:10:11 -0400

    Add AppInternalInfo class
    This will be helpful for sharing a limited subset of data between admin
    and app clients about a particular app. The main use case is to provide
    custom converter info to the admin client when running inside the same
    process as an app client.

---
+++ NFig/AppInternalInfo.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using JetBrains.Annotations;
+using NFig.Converters;
+
+namespace NFig
+{
+    class AppInternalInfo
+    {
+        static readonly Dictionary<string, AppInternalInfo> s_infoByApp = new Dictionary<string, AppInternalInfo>();
+
+        internal string AppName { get; }
+        [CanBeNull]
+        internal Type SettingsType { get; private set; }
+        [CanBeNull]
+        internal Dictionary<Type, ISettingConverter> Converters { get; private set; }
+
+        AppInternalInfo(string appName, Type settingsType)
+        {
+            AppName = appName;
+            SettingsType = settingsType;
+        }
+
+        internal static AppInternalInfo GetAppInfo(string appName, Type settingsType = null)
+        {
+            if (appName == null)
+                throw new ArgumentNullException(nameof(appName));
+
+            lock (s_infoByApp)
+            {
+                AppInternalInfo info;
+                if (s_infoByApp.TryGetValue(appName, out info))
+                {
+                    if (settingsType != null) // called from an AppClient
+                    {
+                        if (info.SettingsType == null)
+                        {
+                            // This is the first time an app client has been setup for this particular app. We'll just blindly trust that they picked the right TSettings.
+                            info.SettingsType = settingsType;
+                        }
+                        else if (settingsType != info.SettingsType)
+                        {
+                            // there is a mismatch between 
+                            var ex = new NFigException($"The TSettings of app \"{appName}\" does not match the TSettings used when the first NFigAppClient was initialized for the app");
+                            ex.Data["OriginalTSettings"] = info.SettingsType.FullName;
+                            ex.Data["NewTSettings"] = settingsType.FullName;
+                            throw ex;
+                        }
+                    }
+
+                    return info;
+                }
+
+                info = new AppInternalInfo(appName, settingsType);
+                s_infoByApp[appName] = info;
+                return info;
+            }
+        }
+
+        internal void SetConverters(Dictionary<Type, ISettingConverter> converters)
+        {
+            Converters = converters;
+        }
+    }
+}
