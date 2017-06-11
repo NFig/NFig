9c118cfdbcfdcab2c9e574d720b55fee71aa36f0
Bret Copeland <bret@atlantisflight.org>
2017-06-11 15:06:57 -0400

    Add AppMetadata and SubAppMetadata models

---
+++ NFig/Metadata/AppMetadata.cs
@@ -0,0 +1,50 @@
+﻿namespace NFig.Metadata
+{
+    /// <summary>
+    /// Metadata about an NFig application. This provides most of the information that an admin panel needs for an app. However, it does not include default
+    /// values. For those, see <see cref="SubAppMetadata{TTier,TDataCenter}"/>.
+    /// </summary>
+    public class AppMetadata
+    {
+        /// <summary>
+        /// The name of the application.
+        /// </summary>
+        public string AppName { get; }
+        /// <summary>
+        /// Information about the current tier.
+        /// </summary>
+        public EnumValue CurrentTier { get; }
+        /// <summary>
+        /// Information about the current data center.
+        /// </summary>
+        public EnumValue CurrentDataCenter { get; }
+        /// <summary>
+        /// Information about the available data centers.
+        /// </summary>
+        public EnumMetadata DataCenterMetadata { get; }
+        /// <summary>
+        /// Basic metadata about each setting.
+        /// </summary>
+        public BySetting<SettingMetadata> MetadataBySetting { get; }
+        /// <summary>
+        /// A list of sub-apps which are registered for this application.
+        /// </summary>
+        public SubApp[] SubApps { get; }
+
+        internal AppMetadata(
+            string appName,
+            EnumValue currentTier,
+            EnumValue currentDataCenter,
+            EnumMetadata dataCenterMetadata,
+            BySetting<SettingMetadata> metadataBySetting,
+            SubApp[] subApps)
+        {
+            AppName = appName;
+            CurrentTier = currentTier;
+            CurrentDataCenter = currentDataCenter;
+            DataCenterMetadata = dataCenterMetadata;
+            MetadataBySetting = metadataBySetting;
+            SubApps = subApps;
+        }
+    }
+}
---
+++ NFig/Metadata/SubAppMetadata.cs
@@ -0,0 +1,36 @@
+﻿namespace NFig.Metadata
+{
+    /// <summary>
+    /// Metadata specific to a sub-app. This includes the default values applicable to the sub-app.
+    /// </summary>
+    public class SubAppMetadata<TTier, TDataCenter>
+        where TTier : struct
+        where TDataCenter : struct
+    {
+        /// <summary>
+        /// The name of the root application.
+        /// </summary>
+        public string AppName { get; }
+        /// <summary>
+        /// The ID of the sub-app, or null if this is the root application.
+        /// </summary>
+        public int? SubAppId { get; }
+        /// <summary>
+        /// The name of the sub-app, or null if this is the root application.
+        /// </summary>
+        public string SubAppName { get; }
+        /// <summary>
+        /// The default values applicable to each setting. Each setting will have one or more default values. Only defaults applicable to the current tier are
+        /// included.
+        /// </summary>
+        public ListBySetting<DefaultValue<TTier, TDataCenter>> DefaultsBySetting { get; }
+
+        internal SubAppMetadata(string appName, int? subAppId, string subAppName, ListBySetting<DefaultValue<TTier, TDataCenter>> defaultsBySetting)
+        {
+            AppName = appName;
+            SubAppId = subAppId;
+            SubAppName = subAppName;
+            DefaultsBySetting = defaultsBySetting;
+        }
+    }
+}
