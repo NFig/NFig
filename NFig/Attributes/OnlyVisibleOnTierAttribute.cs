69bf74151b207ebaf2d30bd2557ced6cd9a6d326
Bret Copeland <bret@atlantisflight.org>
2017-06-11 03:02:37 -0400

    Add OnlyVisibleOnTierAttribute
    This will be used to mark certain data centers as only applicable to
    certain tiers.

---
+++ NFig/Attributes/OnlyVisibleOnTierAttribute.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace NFig
+{
+    /// <summary>
+    /// Allows you to mark certain enum values as only visible to a particular tier(s). The main use case is to annotate the DataCenter enum values since the
+    /// list of data centers will likely vary per tier.
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Field)]
+    public class OnlyVisibleOnTierAttribute : Attribute
+    {
+        /// <summary>
+        /// The tier(s) where the value is visible.
+        /// </summary>
+        public object[] Tiers { get; }
+
+        /// <summary>
+        /// Allows you to mark certain enum values as only visible to a particular tier(s). The main use case is to annotate the DataCenter enum values since the
+        /// list of data centers will likely vary per tier.
+        /// </summary>
+        /// <param name="tiers">
+        /// One or more tiers. The type of each must be the same as the generic TTier argument used when instantiating
+        /// <see cref="NFigStore{TTier,TDataCenter}"/>, otherwise they will be ignored.
+        /// </param>
+        public OnlyVisibleOnTierAttribute(params object[] tiers)
+        {
+            Tiers = tiers;
+        }
+    }
+}
